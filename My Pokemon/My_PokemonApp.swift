4753704e149d9d44b65cf3344467de420f669a0f
IsayDBS <holasoyisay@gmail.com>
2023-09-06 20:53:19 -0600

    Initial Commit

---
+++ My Pokemon/My_PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_PokemonApp.swift
+//  My Pokemon
+//
+//  Created by Isay Damar Balderas Salomón on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct My_PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
